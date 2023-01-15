f00f9c06fd110fa4add91c7e1b180692d0408a29
anasilva <anacmas@gmail.com>
2023-01-14 23:36:06 -0300

    feat: add event listener and function to the btn

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const button = document.querySelector(".button-generate");
 const hex = document.querySelector(".hex");
+
+button.addEventListener("click", generateHex);
+
+function generateHex() {
+  let color = Math.random();
+  let colorMultiplied = Math.trunc(color * 10000000).toString(16);
+  hex.innerHTML = "#" + colorMultiplied;
+  document.body.style.backgroundColor = "#" + colorMultiplied;
+}
